3cbb487ae44a68cc672c5d7452ba2ed7c0dba89c
Randy-RM <randymuhema@gmail.com>
2021-05-06 12:02:47 +0100

    feat : add route system in app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,27 @@
+import { Route, Switch } from "react-router-dom";
+
 let App = () => {
 
     return (
         <>
             <h1>Hello World !!!</h1>
+            <Switch>
+                <Route path="/detail">
+                    <RobotDetail />
+                </Route>
+                <Route path="/upcoming-movies">
+                    <RobotDetail />
+                </Route>
+                <Route path="/tv-show">
+                    <RobotDetail />
+                </Route>
+                <Route path="/movies">
+                    <RobotDetail />
+                </Route>
+                <Route path="/">
+                    <RobotList />
+                </Route>
+            </Switch>
         </>
     );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,14 @@
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
-ReactDOM.render(<><App /></>, document.querySelector("#root"));
+let history = createBrowserHistory();
+
+ReactDOM.render(
+    <>
+        <Router history={history}>
+            <App />
+        </Router>
+    </>
+    , document.querySelector("#root"));
